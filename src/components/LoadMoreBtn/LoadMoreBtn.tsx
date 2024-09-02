55962f2e817b093b7b709e36671cd86325097c55
Anatolii1203 <v120386t@gmail.com>
2024-09-02 18:56:47 +0300

    typescript-2

---
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -0,0 +1,13 @@
+import s from "./LoadMoreBtn.module.css";
+
+interface LoadMoreBtnType {
+  onClick: () => void;
+}
+
+export const LoadMoreBtn: React.FC<LoadMoreBtnType> = ({ onClick }) => {
+  return (
+    <div className={s.loadBtn}>
+      <button onClick={onClick}>Load more</button>
+    </div>
+  );
+};
